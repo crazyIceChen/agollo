899343864832e947c1e88c1b076ab37cdc0c475b
陈顾文 <chenguwen@cmiot.chinamobile.com>
2019-04-11 09:47:23 +0800

    modify log to logrus

--- log.go
+++ log.go
@@ -1,13 +1,13 @@
 package agollo
 
 import (
-	"github.com/cihub/seelog"
+	"github.com/sirupsen/logrus"
 )
 
 var logger LoggerInterface
 
 func init() {
-	initLogger(initSeeLog("seelog.xml"))
+	initLogger(initLogrusLog())
 }
 
 func initLogger(ILogger LoggerInterface) {
@@ -19,30 +19,20 @@ type LoggerInterface interface {
 
 	Infof(format string, params ...interface{})
 
-	Warnf(format string, params ...interface{}) error
+	Warnf(format string, params ...interface{})
 
-	Errorf(format string, params ...interface{}) error
+	Errorf(format string, params ...interface{})
 
 	Debug(v ...interface{})
 
 	Info(v ...interface{})
 
-	Warn(v ...interface{}) error
+	Warn(v ...interface{})
 
-	Error(v ...interface{}) error
+	Error(v ...interface{})
 }
 
-func initSeeLog(configPath string) (LoggerInterface) {
-	logger, err := seelog.LoggerFromConfigAsFile(configPath)
-
-	//if error is happen change to default config.
-	if err != nil {
-		logger, err = seelog.LoggerFromConfigAsBytes([]byte("<seelog />"))
-	}
-
-	logger.SetAdditionalStackDepth(1)
-	seelog.ReplaceLogger(logger)
-	defer seelog.Flush()
-
+func initLogrusLog() LoggerInterface {
+	logrus.New()
 	return logger
 }
